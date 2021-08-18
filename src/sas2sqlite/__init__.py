014e32dc14b5efbcb22de84f9c0e8a3de6d63974
Eric Gjertsen <ericgj72@gmail.com>
2021-08-18 11:31:45 -0400

    implement CLI
      - move date/time/datetime sqlite adapters to adapters module
      - move all sqlite adapter logic from __init__ to __main__
      - remove schema option (doesn't make sense for sqlite here)
      - rewrite tests against the CLI (test_cli), disable earlier tests
    
    Note sqlite adapter settings were moved to the CLI vs. the library code,
    because the assumption is using the library you bring your own config of
    sqlite3 -- you are not limited to the few preset adapters I set up for
    date/time/datetime data. Likewise with the config of the sas7bdat parser.
    The library assumes you set that up externally. (The one exception being
    that it forces skip_header=True, which is always what we want for
    importing to a dbase.)

--- src/sas2sqlite/__init__.py
+++ src/sas2sqlite/__init__.py
@@ -1,84 +1,9 @@
 __version__ = "0.0.1"
 
-from calendar import timegm
-from datetime import date, datetime, time
 import sqlite3
-from typing import Any, Optional, Iterable, Tuple, List, Callable
+from typing import Any, Optional, Iterable, Tuple, List
 
-import julian  # type: ignore
 from sas7bdat import SAS7BDAT  # type: ignore
-
-
-def time_to_seconds(t: time) -> float:
-    return (60 * 60 * t.hour) + (60 * t.minute) + t.second + t.microsecond
-
-
-def time_to_text(format: str) -> Callable[[time], str]:
-    def _time_to_text(t: time) -> str:
-        return t.strftime(format)
-
-    return _time_to_text
-
-
-def date_to_posix(d: date) -> int:
-    return datetime_to_posix(datetime(d.year, d.month, d.day))
-
-
-def date_to_julian(d: date) -> float:
-    return datetime_to_julian(datetime(d.year, d.month, d.day))
-
-
-def date_to_text(format: str) -> Callable[[date], str]:
-    def _date_to_text(d: date) -> str:
-        return d.strftime(format)
-
-    return _date_to_text
-
-
-def datetime_to_posix(dt: datetime) -> int:
-    return timegm(dt.utctimetuple())
-
-
-def datetime_to_julian(dt: datetime) -> float:
-    return float(julian.to_jd(dt))
-
-
-def datetime_to_text(format: str) -> Callable[[datetime], str]:
-    def _datetime_to_text(dt: datetime) -> str:
-        return dt.strftime(format)
-
-    return _datetime_to_text
-
-
-def store_time(time_type: str, time_format: str) -> None:
-    if time_type == "seconds":
-        sqlite3.register_adapter(time, time_to_seconds)
-    elif time_type == "text":
-        sqlite3.register_adapter(time, time_to_text(time_format))
-    else:
-        raise ValueError(f"Unknown time adapter: '{time_type}'")
-
-
-def store_date(date_type: str, date_format: str) -> None:
-    if date_type == "julian":
-        sqlite3.register_adapter(date, date_to_julian)
-    elif date_type == "posix":
-        sqlite3.register_adapter(date, date_to_posix)
-    elif date_type == "text":
-        sqlite3.register_adapter(date, date_to_text(date_format))
-    else:
-        raise ValueError(f"Unknown date adapter: '{date_type}'")
-
-
-def store_datetime(datetime_type: str, datetime_format: str) -> None:
-    if datetime_type == "julian":
-        sqlite3.register_adapter(datetime, datetime_to_julian)
-    elif datetime_type == "posix":
-        sqlite3.register_adapter(datetime, datetime_to_posix)
-    elif datetime_type == "text":
-        sqlite3.register_adapter(datetime, datetime_to_text(datetime_format))
-    else:
-        raise ValueError(f"Unknown datetime adapter: '{datetime_type}'")
 
 
 def import_dataset(
@@ -86,20 +11,11 @@ def import_dataset(
     dataset: SAS7BDAT,
     *,
     table: Optional[str] = None,
-    schema: Optional[str] = None,
     create_table: bool = False,
-    store_time_as: str = "text",
-    store_time_format: str = "%H:%M:%S",
-    store_date_as: str = "text",
-    store_date_format: str = "%Y-%m-%d",
-    store_datetime_as: str = "text",
-    store_datetime_format: str = "%Y-%m-%dT%H:%M:%S",
+    sqlite_time_type: str = "TEXT",
+    sqlite_date_type: str = "TEXT",
+    sqlite_datetime_type: str = "TEXT",
 ):
-    saved_adapters = sqlite3.adapters.copy()
-    store_time(store_time_as, store_time_format)
-    store_date(store_date_as, store_date_format)
-    store_datetime(store_datetime_as, store_datetime_format)
-
     saved_skip_header = dataset.skip_header
     dataset.skip_header = True  # always skip header row for our purposes
 
@@ -110,19 +26,18 @@ def import_dataset(
                 table = r.header.properties.name.decode("utf-8")
             with conn:
                 if create_table:
-                    conn.execute(drop_table_sql(table, schema))
+                    conn.execute(drop_table_sql(table))
                     conn.execute(
                         create_table_sql(
                             table,
                             cols,
-                            schema=schema,
                             encoding=r.encoding,
                             sas_time_formats=r.TIME_FORMAT_STRINGS,
                             sas_date_formats=r.DATE_FORMAT_STRINGS,
                             sas_datetime_formats=r.DATE_TIME_FORMAT_STRINGS,
-                            store_time_as=store_time_as,
-                            store_date_as=store_date_as,
-                            store_datetime_as=store_datetime_as,
+                            sqlite_time_type=sqlite_time_type,
+                            sqlite_date_type=sqlite_date_type,
+                            sqlite_datetime_type=sqlite_datetime_type,
                         )
                     )
 
@@ -130,19 +45,18 @@ def import_dataset(
                 for row in r:
                     conn.execute(
                         *insert_sql(
-                            table, cols, row, encoding=r.encoding, schema=schema
+                            table,
+                            cols,
+                            row,
+                            encoding=r.encoding,
                         )
                     )
     finally:
-        sqlite3.adapters = saved_adapters
         dataset.skip_header = saved_skip_header
 
 
-def drop_table_sql(table: str, schema: Optional[str] = None) -> str:
-    if schema is None:
-        return f"DROP TABLE IF EXISTS `{table}`;"
-    else:
-        return f"DROP TABLE IF EXISTS `{schema}`.`{table}`;"
+def drop_table_sql(table: str) -> str:
+    return f"DROP TABLE IF EXISTS `{table}`;"
 
 
 def create_table_sql(
@@ -153,10 +67,9 @@ def create_table_sql(
     sas_time_formats: Iterable[str],
     sas_date_formats: Iterable[str],
     sas_datetime_formats: Iterable[str],
-    store_time_as: str,
-    store_date_as: str,
-    store_datetime_as: str,
-    schema: Optional[str] = None,
+    sqlite_time_type: str,
+    sqlite_date_type: str,
+    sqlite_datetime_type: str,
 ) -> str:
     col_lines: List[str] = []
     for col in cols:
@@ -164,12 +77,14 @@ def create_table_sql(
             col_lines.append(f"`{col.name.decode(encoding)}` VARCHAR({col.length})")
         elif col.type.lower() == "number":
             format = None if col.format is None else col.format.upper()
-            if not format is None and (
-                (format in sas_time_formats and store_time_as == "text")
-                or (format in sas_date_formats and store_date_as == "text")
-                or (format in sas_datetime_formats and store_datetime_as == "text")
-            ):
-                col_lines.append(f"`{col.name.decode(encoding)}` TEXT")
+            if format is not None and format in sas_time_formats:
+                col_lines.append(f"`{col.name.decode(encoding)}` {sqlite_time_type}")
+            elif format is not None and format in sas_date_formats:
+                col_lines.append(f"`{col.name.decode(encoding)}` {sqlite_date_type}")
+            elif format is not None and format in sas_datetime_formats:
+                col_lines.append(
+                    f"`{col.name.decode(encoding)}` {sqlite_datetime_type}"
+                )
             else:
                 col_lines.append(f"`{col.name.decode(encoding)}` NUMERIC")
         else:
@@ -178,10 +93,7 @@ def create_table_sql(
             )
 
     col_defs = ", ".join(col_lines)
-    if schema is None:
-        return f"CREATE TABLE `{table}` ({col_defs});"
-    else:
-        return f"CREATE TABLE `{schema}`.`{table}` ({col_defs});"
+    return f"CREATE TABLE `{table}` ({col_defs});"
 
 
 def insert_sql(
@@ -189,15 +101,8 @@ def insert_sql(
     cols: Iterable[Any],
     row: Iterable[Any],
     *,
-    encoding,
-    schema: Optional[str] = None,
+    encoding: str,
 ) -> Tuple[str, Iterable[Any]]:
     col_expr = ",".join([f"`{col.name.decode(encoding)}`" for col in cols])
     val_expr = ",".join(["?" for col in cols])
-    if schema is None:
-        return (f"INSERT INTO `{table}` ({col_expr}) VALUES ({val_expr});", row)
-    else:
-        return (
-            f"INSERT INTO `{schema}`.`{table}` ({col_expr}) VALUES ({val_expr});",
-            row,
-        )
+    return (f"INSERT INTO `{table}` ({col_expr}) VALUES ({val_expr});", row)
